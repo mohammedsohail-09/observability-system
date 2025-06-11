f69fd6eb77c02681b72551a4ff112bfc5e5b37a4
Ubuntu <ubuntu@ip-172-31-33-86.ap-south-1.compute.internal>
2025-06-11 17:55:04 +0000

    Initial commit

---
+++ app/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+app = Flask(__name__)
+FlaskInstrumentor().instrument_app(app)
+
+# Setup tracing
+trace.set_tracer_provider(TracerProvider())
+jaeger_exporter = JaegerExporter(
+    agent_host_name="jaeger",
+    agent_port=6831,
+)
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(jaeger_exporter)
+)
+
+@app.route('/')
+def hello():
+    return "Hello from Observability App!"
